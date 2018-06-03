b15c5fc70e72b58f38c3acaa327a5921260e23c4
dmytryK <madj90@gmail.com>
2018-06-03 18:16:03 +0200

    Navigation was added through lambda and navigate

--- app/src/main/java/com/example/dimi/navigation/StepOneFragment.kt
+++ app/src/main/java/com/example/dimi/navigation/StepOneFragment.kt
@@ -7,6 +7,8 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_step_one.*
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
@@ -15,7 +17,6 @@ class StepOneFragment : Fragment() {
 
     private var param1: String? = null
     private var param2: String? = null
-    private var listener: OnFragmentInteractionListener? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,30 +32,15 @@ class StepOneFragment : Fragment() {
     ): View? =
         inflater.inflate(R.layout.fragment_step_one, container, false)
 
-    fun onButtonPressed(uri: Uri) {
-        listener?.onFragmentInteraction(uri)
-    }
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        if (context is OnFragmentInteractionListener) {
-            listener = context
-        } else {
-            throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        goToNextButton?.setOnClickListener {
+            Navigation.findNavController(it)
+                .navigate(R.id.action_stepOneFragment_to_stepTwoFragment)
         }
     }
 
-    override fun onDetach() {
-        super.onDetach()
-        listener = null
-    }
-
-    interface OnFragmentInteractionListener {
-        fun onFragmentInteraction(uri: Uri)
-    }
-
     companion object {
-
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             StepOneFragment().apply {
