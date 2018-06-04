358926aee4be5956b00c09047b4236023939c55e
Dmytro Kuiko <dmytro.kuiko@incloud.de>
2018-06-04 10:27:09 +0200

    Logic for toolbar was added

--- app/src/main/java/com/example/dimi/navigation/MainActivity.kt
+++ app/src/main/java/com/example/dimi/navigation/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.dimi.navigation
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
@@ -16,10 +18,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        toolbar?.let {
+            it.inflateMenu(R.menu.toolbar_navigation)
+            setSupportActionBar(it)
+        }
+
         safeLet(bottomNavigationView, navHostFragment) { bottomNav, navHost ->
             NavigationUI.setupWithNavController(bottomNav, navHost.navController)
         }
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean =
+        menuInflater.inflate(R.menu.toolbar_navigation, menu).run {
+            true
+        }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean =
+        NavigationUI.onNavDestinationSelected(
+            item,
+            Navigation.findNavController(this, R.id.navHostFragment)
+        ) || super.onOptionsItemSelected(item)
+
+
+    override fun onSupportNavigateUp(): Boolean =
+        Navigation.findNavController(this, R.id.navHostFragment).navigateUp()
+
 }
 
 fun <T1 : Any, T2 : Any, R : Any> safeLet(t1: T1?, t2: T2?, block: (T1, T2) -> R?): R? =
