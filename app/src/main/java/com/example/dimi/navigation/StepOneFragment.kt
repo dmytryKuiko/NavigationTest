89c7b939a9126dbf85ee2e40abdadea3fdbd1a91
dmytryK <madj90@gmail.com>
2018-06-10 22:54:31 +0200

    StepFragments code was cleaned

--- app/src/main/java/com/example/dimi/navigation/StepOneFragment.kt
+++ app/src/main/java/com/example/dimi/navigation/StepOneFragment.kt
@@ -1,7 +1,5 @@
 package com.example.dimi.navigation
 
-import android.content.Context
-import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
@@ -10,21 +8,7 @@ import android.view.ViewGroup
 import androidx.navigation.Navigation
 import kotlinx.android.synthetic.main.fragment_step_one.*
 
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 class StepOneFragment : Fragment() {
-
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -39,24 +23,14 @@ class StepOneFragment : Fragment() {
             safeArgs.someName
         }
 
-        val valueFromBundle = arguments?.let {
-            it.getInt("someIntManual")
-        }
+        val valueFromBundle = arguments?.getInt("someIntManual")
 
         goToNextButton?.setOnClickListener {
             Navigation.findNavController(it)
                 .navigate(R.id.action_stepOneFragment_to_stepTwoFragment)
         }
-    }
-
-    companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            StepOneFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        backButton.setOnClickListener {
+            Navigation.findNavController(it).popBackStack(R.id.homeFragment, false)
+        }
     }
 }
--- app/src/main/java/com/example/dimi/navigation/StepTwoFragment.kt
+++ app/src/main/java/com/example/dimi/navigation/StepTwoFragment.kt
@@ -1,32 +1,15 @@
 package com.example.dimi.navigation
 
-import android.content.Context
-import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.navigation.NavOptions
 import androidx.navigation.Navigation
 import kotlinx.android.synthetic.main.fragment_step_two.*
 
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 class StepTwoFragment : Fragment() {
-
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -46,16 +29,8 @@ class StepTwoFragment : Fragment() {
             Navigation.findNavController(it)
                 .navigate(R.id.action_stepTwoFragment_to_homeFragment, null, options)
         }
-    }
-
-    companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            StepTwoFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        backButton?.setOnClickListener {
+            Navigation.findNavController(it).popBackStack(R.id.homeFragment, false)
+        }
     }
 }
